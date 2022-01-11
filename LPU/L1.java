95165d6ab39366f23756655f6959a6f9e0cad030
nikhil051102 <nikhildeore05@gmail.com>
2022-01-11 11:38:05 +0530

    -

--- LPU/L1.java
+++ LPU/L1.java
@@ -1,5 +1,5 @@
-// public class L1 {    //Our file should be saved with ClassName.java
-//     public static void main(String[] args) {   //"S" of string should be capitalized
-//         System.out.println("Hello World");    //"S" of system should be capitalized
-//     }
-// }
+public class L1 {    //Our file should be saved with ClassName.java
+    public static void main(String[] args) {   //"S" of string should be capitalized
+        System.out.println("Hello World");    //"S" of system should be capitalized
+    }
+}
