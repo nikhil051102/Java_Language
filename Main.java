ba8d1fe6a1ffedea63ddd61e253eee437c69fd4e
nikhil051102 <nikhildeore05@gmail.com>
2022-03-09 15:38:31 +0530

    o

--- Main.java
+++ Main.java
@@ -3,17 +3,14 @@ import java.util.Scanner;  // Import the Scanner class
 class Main {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter first no");
-    int a = myObj.nextInt();  // Read user input
-    System.out.println("Enter second no");
-    int b = myObj.nextInt();  // Read user input
-    int sum = a + b;
-    System.out.println("Sum of " + a + " and " + b + " is " + sum);
-    int difference = a - b;
-    System.out.println("Difference of " + a + " and " + b + " is " + difference);
-    int product = a * b;
-    System.out.println("Product of " + a + " and " + b + " is " + product);
-    int quotient = a / b;
-    System.out.println("Quotient of " + a + " and " + b + " is " + quotient);
+    // Swap two variables without using a third variable
+    System.out.println("Enter two numbers: ");
+    int a = myObj.nextInt();
+    int b = myObj.nextInt();
+    System.out.println("Before swapping: " + a + " " + b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    System.out.println("After swapping: " + a + " " + b);
   }
 }
